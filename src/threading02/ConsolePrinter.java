1642ef3403e37531834bc295ed5e302928e0bbc6
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-04-23 13:52:02 +0200

    Threading02

--- src/threading02/ConsolePrinter.java
+++ src/threading02/ConsolePrinter.java
@@ -1,6 +1,6 @@
 package threading02;
 
-public class ConsolePrinter {
+public class ConsolePrinter implements Runnable {
 
     private String identifier;
 
@@ -8,11 +8,20 @@ public class ConsolePrinter {
         this.identifier = identifier;
     }
 
-    public void logic() {
+    private void logic() throws InterruptedException {
         for (int i = 0; i < 1_000; i++) {
             System.out.println(i + ": [identifier: " + identifier + ']');
 
-            //Thread.sleep(100);
+            Thread.sleep(10);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            logic();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
